3d8c5e78e58ab655c0346f3ad64c800870a290f5
Alex Faggian <afaggian@gmail.com>
2023-07-27 00:38:48 +1000

    [WIP] babel and adding history

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -43,7 +43,7 @@ export default function Form() {
           </div>
         </div>
       </form>
-	  <List><List/>
+      <List></List>
     </div>
   );
 }
---
+++ client/src/components/List.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const obj = [
+  {
+    name: "Savings",
+    color: "rgb(255, 99, 132)",
+  },
+  {
+    type: "Investment",
+    color: "#f9c74f",
+  },
+  {
+    name: "Expense",
+    color: "rgb(54, 162, 235)",
+  },
+];
+
+export default function List() {
+  return (
+    <div className="flex flex-col py-6 gap-3">
+      <h1 className="py-4 font-bold text-xl">History</h1>
+      {obj.map((value, index) => (
+        <Transaction key={index} category={value}></Transaction>
+      ))}
+    </div>
+  );
+}
+
+function Transaction({ category }) {
+  if (!category) return null;
+  return (
+    <div
+      className="item flex justify-center bg-gray-50 py-2 rounded-r"
+      style={{ borderRight: `8px solid ${category} ` }}
+    >
+      <span className="block w-full">{category.name ?? ""}</span>
+    </div>
+  );
+}
